70cc09c664be61e7cb6a0d3a675d831eb2c36224
GeetikaNahak <geetikasaisravaninahak@gmail.com>
2025-07-17 19:37:33 +0530

    Header

--- app/page.tsx
+++ app/page.tsx
@@ -15,50 +15,9 @@ import CampustLife from "./research/campusLife"
 import { useEffect, useRef, useState } from "react";
 import gsap from 'gsap';
 
-const slides = [
-  {
-    type: 'image',
-    src: '/image2.jpg',
-    title: 'CBN x VIT-AP University',
-    subtitle: 'V‑LaunchPad 2025 @ VIT‑AP University®',
-    link: '#',
-  },
-  {
-    type: 'image',
-    src: '/image4.jpg',
-    title: 'Explore Innovation',
-    subtitle: 'Empowering the Future at VIT‑AP',
-    link: '#',
-  },
-  {
-    type: 'image',
-    src: '/image2.jpg',
-    link: 'https://vitap.ac.in/ug-pg-admissions',
-  },
-  {
-    type: 'video',
-    src: '/image3.jpg',
-    title: 'Welcome to VIT‑AP University',
-    subtitle: 'APPLY KNOWLEDGE, IMPROVE LIFE®',
-    link: '#',
-  },
-];
+
 
 export default function HomePage() {
-  const container = useRef<HTMLDivElement>(null);
-  const [index, setIndex] = useState(0);
-
-  const prev = () => setIndex(i => (i - 1 + slides.length) % slides.length);
-  const next = () => setIndex(i => (i + 1) % slides.length);
-
-  useEffect(() => {
-    if (!container.current) return;
-    gsap.to(container.current, {
-      x: `-${index * 100}%`,
-      duration: 0.8,
-      ease: 'power2.inOut',
-    });
-  }, [index]);
 
   return (
     <div className="min-h-screen">
